29a284b8a3def03dedc401967860b48e86eb0b69
Kreytal <anoslepums@gmail.com>
2023-03-06 19:17:36 +0000

    Added parameters to the methods used in Testing class

--- Testing.cs
+++ Testing.cs
@@ -13,10 +13,18 @@ namespace CMP1903M_A01_2223
             Pack pack = new Pack();
             Validation validation = new Validation();
             Console.WriteLine("How would you like to shuffle a pack of cards? 1. Fisher Yanes; 2. Riffle Shuffle; 3. No Shuffle");
-            int userInput = validation.intInputValidation();
+            int userInput = validation.intInputValidation(1, 3);
             bool shuffled = pack.shuffleCardPack(userInput);
             Console.WriteLine("Now choose whether you want to: 1. Deal one card; 2. Deal cards by amount");
+            int dealChoice = validation.intInputValidation(1, 2);
+            if (dealChoice == 1)
+            {
 
+            }
+            if (dealChoice == 2)
+            {
+
+            }
             
         }
     }
